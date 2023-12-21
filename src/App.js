1ff7724e65f1610764efd70f65b0618704a842db
BanuDanabakiyam <banubala9655@gmail.com>
2023-12-21 16:08:04 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter,Route, Routes } from "react-router-dom";
 import RegisterPage from "./components/pages/Register";
+import LoginPage from "./components/pages/Login";
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
+        <Route path="login" element={<LoginPage/>}/>
         <Route path="register" element={<RegisterPage/>}></Route>
         <Route path="/" element={<h1>Home</h1>}/>
       </Routes>
